9c3ab31330b03fa81417a1974f78de73206ab6f8
GUSTAVO GEWEHR SOARES <52550134+gusgewehr@users.noreply.github.com>
2023-05-04 00:03:48 -0300

    models

--- eventos/models.py
+++ eventos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Evento(models.Model):
+    nome = models.CharField(max_length=500)
+    descricao = models.CharField(max_length=4000)
+    preco_ingresso = models.DecimalField(decimal_places=2)
+    ingresso_meia = models.BooleanField()
+    ingresso_comunitario = models.BooleanField()
+
+
