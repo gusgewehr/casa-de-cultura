cd2efaca0faba110fe5e1643afabb648c3acaf9a
GUSTAVO GEWEHR SOARES <52550134+gusgewehr@users.noreply.github.com>
2023-05-08 19:49:09 +0000

    add_dates

--- eventos/models.py
+++ eventos/models.py
@@ -8,4 +8,16 @@ class Evento(models.Model):
     ingresso_meia = models.BooleanField()
     ingresso_comunitario = models.BooleanField()
 
+class EventDates(models.Model):
+    evento = models.ForeignKey(Evento, on_delete = models.CASCADE)
+    date = models.DateTimeField()
+    type_choices = (
+        ("EN", "Ensaio"),
+        ("MNT","Montagem do Palco"),
+        ("AP","Apresentação"),
+        ("DMNT","Desmontagem do Palco"),
+    )
+    uso = models.CharField(max_length=5, choices=type_choices, default="AP")
+
+
 
