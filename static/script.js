cb965e91c2b223237eb27eee6afa9d869fddf86e
gustavo.soares <gustavo.soares@stara.com.br>
2023-05-10 16:58:43 -0300

    new image

--- static/script.js
+++ static/script.js
@@ -18,8 +18,10 @@ function add_date(){
 
     var filho = document.getElementById("data_groupo_1")
     var new_date_input = document.createElement("input")
+    new_date_input.setAttribute("type", "date")
     new_date_input.setAttribute("class", "block w-full rounded-md border-0 px-3.5 py-2 pl-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6")
     filho.appendChild(new_date_input)
+
 
 
     number_of_dates++
